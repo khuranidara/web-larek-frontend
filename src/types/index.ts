2ccf9d8088b782a4d5c3d4cb6f452217d25fcd6b
d.khurani <huranidara@gmail.com>
2024-02-11 19:07:55 +0300

    Исправление согласно требованиям ревьюера 2
    Реализация типов данных

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,73 @@
-import { Product, Customer } from './models';
-import { CartActionTypes, CheckoutActionTypes } from './actions';
+// Модели данных
+
+interface Product {
+	productId: string;
+	description: string;
+	category: string;
+	title: string;
+	imageUrl: string;
+	price: number;
+}
+
+interface Customer {
+	id: string;
+	payType: number;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+// Действия с товарами
+
+interface ProductsActions {
+	getAvailableProducts(): Product[];
+	getProduct(id: string): Product | null;
+}
+
+// Действия с корзиной
+
+interface CartActions {
+	openCartWindow(): void;
+	closeCartWindow(): void;
+	addProductToCart(id: string): void;
+	removeProductFromCart(index: number): void;
+	calculateOverallProductsPrice(): number;
+}
+
+// Действия с оформлением заказа
+
+interface CheckoutActions {
+	openCheckoutWindow(): void;
+	closeCheckoutWindow(): void;
+	updatePayType(type: number): void;
+	updateAddress(address: string): void;
+	updateEmail(email: string): void;
+	updatePhone(phone: string): void;
+	proceedToPayment(): void;
+}
+
+// Представления
+
+interface HomeView {
+	displayProducts(products: Product[]): void;
+}
+
+interface CardView {
+	// Можно расширить интерфейс, если нужно
+}
+
+interface CartView {
+	// Можно расширить интерфейс, если нужно
+}
+
+interface CheckoutPaymentView {
+	// Можно расширить интерфейс, если нужно
+}
+
+interface CheckoutContactInfoView {
+	// Можно расширить интерфейс, если нужно
+}
+
+interface CheckoutResultView {
+	// Можно расширить интерфейс, если нужно
+}
