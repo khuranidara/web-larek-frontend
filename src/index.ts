948a1598a56a10e227dd0d40b40b612c460feb8c
d.khurani <huranidara@gmail.com>
2024-03-01 20:31:40 +0300

    Ответ с свервера получен

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,27 @@ import * as querystring from 'querystring';
 import { Customer } from './types/Customer';
 
 const gallery = document.querySelector('.gallery');
-const cardtemp = document.querySelector("#card-catalog") as HTMLTemplateElement;
+const cardtemp = document.querySelector('#card-catalog') as HTMLTemplateElement;
 const api = new Api(API_URL);
 let productItems: ProductItems = {} as ProductItems;
-function cloneTemplate(){
 
-api.get("/product/").then( result => {
+function cloneTemplate() {
 
-	productItems = result as ProductItems;
-	console.log(productItems.items);
+	api.get('/product/').then(result => {
 
-	for (let i = 0; i < productItems.total; i++) {
-		const item: Product = productItems.items[i];
-		gallery.appendChild(renderProductCard(item));
-	}
-});
+		productItems = result as ProductItems;
+		console.log(productItems.items);
+
+		for (let i = 0; i < productItems.total; i++) {
+			const item: Product = productItems.items[i];
+			gallery.appendChild(renderProductCard(item));
+		}
+	});
 }
 
 const previewTemplate = document.querySelector('#card-preview') as HTMLTemplateElement;
-function renderProductCard(product: Product):DocumentFragment  {
+
+function renderProductCard(product: Product): DocumentFragment {
 	const catalog_clone = document.importNode(cardtemp.content, true);
 
 	const category = catalog_clone.querySelector('.card__category');
@@ -37,25 +39,25 @@ function renderProductCard(product: Product):DocumentFragment  {
 
 	category.textContent = product.category;
 	title.textContent = product.title;
-	image.setAttribute('src',  'https://larek-api.nomoreparties.co/content/weblarek' + product.image);
+	image.setAttribute('src', 'https://larek-api.nomoreparties.co/content/weblarek' + product.image);
 	if (price) {
 		if (product.price !== null) {
 			price.textContent = product.price.toString();
 		} else {
-			price.textContent = "Бесценно";
+			price.textContent = 'Бесценно';
 		}
 	}
 	switch (product.category) {
-		case "софт-скил":
+		case 'софт-скил':
 			category.classList.add('card__category_soft');
 			break;
-		case "хард-скил":
+		case 'хард-скил':
 			category.classList.add('card__category_hard');
 			break;
-		case "другое":
+		case 'другое':
 			category.classList.add('card__category_other');
 			break;
-		case "дополнительное":
+		case 'дополнительное':
 			category.classList.add('card__category_additional');
 			break;
 		default:
@@ -63,7 +65,7 @@ function renderProductCard(product: Product):DocumentFragment  {
 			break;
 	}
 	card.addEventListener('click', () => {
-		console.log("card clicked");
+		console.log('card clicked');
 		openFilledPreview(product);
 	});
 
@@ -74,7 +76,7 @@ cloneTemplate();
 
 function openFilledPreview(product: Product) {
 	modalContainer.classList.add('modal_active');
-	modalContainer.style.position = "fixed";
+	modalContainer.style.position = 'fixed';
 	modalContent.innerHTML = '';
 	const previewClone = document.importNode(previewTemplate.content, true);
 	const image = previewClone.querySelector('.card__image');
@@ -91,16 +93,16 @@ function openFilledPreview(product: Product) {
 	modalContent.appendChild(previewClone);
 	if (price) {
 		if (product.price !== null) {
-			price.textContent = product.price.toString();}
-		else {
-			price.textContent = "Бесценно";
+			price.textContent = product.price.toString();
+		} else {
+			price.textContent = 'Бесценно';
 		}
 	}
 	addToBasketButton.addEventListener('click', () => {
 		addToBasket(product.id);
 		upateBasketCounter();
 		modalContainer.classList.remove('modal_active');
-		modalContainer.style.position = "fixed";
+		modalContainer.style.position = 'fixed';
 	});
 };
 
@@ -110,38 +112,41 @@ const modalContainer = document.getElementById('modal-container') as HTMLElement
 const modalContent = modalContainer.querySelector('.modal__content') as HTMLElement;
 
 const basket_button = document.querySelector('.header__basket');
-basket_button.addEventListener("click", function(event) {
+basket_button.addEventListener('click', function(event) {
 	modalContent.innerHTML = '';
 	const basketContent = basketTemplate.content.cloneNode(true);
 	modalContent.appendChild(basketContent);
 	modalContainer.classList.add('modal_active');
-	modalContainer.style.position = "fixed";
+	modalContainer.style.position = 'fixed';
 	fillBasket();
 });
 
 const closeModalButton = modalContainer.querySelector('.modal__close');
-closeModalButton.addEventListener("click", function(event) {
+closeModalButton.addEventListener('click', function(event) {
 	modalContainer.classList.remove('modal_active');
-	modalContainer.style.position = "fixed";
+	modalContainer.style.position = 'fixed';
 });
 
-const basket: string[] =[];
+const basket: string[] = [];
+
 function addToBasket(productId: string) {
 	if (!basket.includes(productId)) {
 		basket.push(productId);
 		upateBasketCounter();
 	}
 }
+
 let totalPrice = 0;
 const orderData = {
 	payment: 'online',
 	email: '',
-	phone:'',
+	phone: '',
 	address: '',
 	total: 0,
-	items: [] as string[]
+	items: [] as string[],
 };
-function fillBasket(){
+
+function fillBasket() {
 	const basketList = document.querySelector('.basket__list');
 	while (basketList.firstChild) {
 		basketList.removeChild(basketList.firstChild);
@@ -150,146 +155,134 @@ function fillBasket(){
 	orderData.items = [];
 	for (let i = 0; i < basket.length; i++) {
 		const productId = basket[i];
-		const product = productItems.items.find(product=> product.id == productId);
-			const cardBasketTemplate = document.getElementById('card-basket') as HTMLTemplateElement;
-			const basketItem = cardBasketTemplate.content.cloneNode(true) as HTMLElement;
-			const indexSpan = basketItem.querySelector('.basket__item-index');
-			const title = basketItem.querySelector('.card__title');
-			const price = basketItem.querySelector('.card__price');
-			const deleteButton = basketItem.querySelector('.basket__item-delete');
-			indexSpan.textContent = `${i+1}`;
-			title.textContent = product.title;
-			if (product.price!== null) {
-				price.textContent = product.price.toString() + " синапсов";
-				totalPrice += product.price;
-			} else {
-				price.textContent = "Бесценно"
-				totalPrice += 0;
-			}
-			deleteButton.addEventListener('click', () =>{
-				basket.splice(i,1);
-				fillBasket();
-				upateBasketCounter();
-			});
-			basketList.appendChild(basketItem);
-			orderData.items.push(productId);
-	}
-		const  totalPriceElement = document.querySelector('.basket__price');
-		totalPriceElement.textContent = `${totalPrice} синапсов`;
-		orderData.total = totalPrice;
-		const checkoutButton = modalContent.querySelector('.basket__button') as HTMLButtonElement;
-		if (basket.length === 0) {
-			checkoutButton.disabled = true;
+		const product = productItems.items.find(product => product.id == productId);
+		const cardBasketTemplate = document.getElementById('card-basket') as HTMLTemplateElement;
+		const basketItem = cardBasketTemplate.content.cloneNode(true) as HTMLElement;
+		const indexSpan = basketItem.querySelector('.basket__item-index');
+		const title = basketItem.querySelector('.card__title');
+		const price = basketItem.querySelector('.card__price');
+		const deleteButton = basketItem.querySelector('.basket__item-delete');
+		indexSpan.textContent = `${i + 1}`;
+		title.textContent = product.title;
+		if (product.price !== null) {
+			price.textContent = product.price.toString() + ' синапсов';
+			totalPrice += product.price;
+		} else {
+			price.textContent = 'Бесценно';
+			totalPrice += 0;
 		}
-		checkoutButton.addEventListener('click', function(event) {
+		deleteButton.addEventListener('click', () => {
+			basket.splice(i, 1);
+			fillBasket();
+			upateBasketCounter();
+		});
+		basketList.appendChild(basketItem);
+		orderData.items.push(productId);
+	}
+	const totalPriceElement = document.querySelector('.basket__price');
+	totalPriceElement.textContent = `${totalPrice} синапсов`;
+	orderData.total = totalPrice;
+	const checkoutButton = modalContent.querySelector('.basket__button') as HTMLButtonElement;
+	if (basket.length === 0) {
+		checkoutButton.disabled = true;
+	}
+	checkoutButton.addEventListener('click', function(event) {
+		modalContent.innerHTML = '';
+		const orderContent = orderTemplate.content.cloneNode(true);
+		modalContent.appendChild(orderContent);
+		modalContainer.classList.add('modal_active');
+		modalContainer.style.position = 'fixed';
+		const orderForm: HTMLFormElement = modalContent.querySelector('.order');
+		const onlinePaymentButton = orderForm.querySelector('button[name="card"]');
+		const cashPaymentButton = orderForm.querySelector('button[name="cash"]');
+		const addressInput: HTMLInputElement = modalContent.querySelector('.form__input');
+		const errorSpan = modalContent.querySelector('.form__errors');
+		const nextButton = modalContent.querySelector('.order__button');
+		let payType = 'online';
+		onlinePaymentButton.addEventListener('click', function() {
+			payType = 'online';
+			orderData.payment = payType;
+			onlinePaymentButton.classList.add('button_alt-active');
+			cashPaymentButton.classList.remove('button_alt-active');
+			console.log('paytype:' + payType);
+		});
+		cashPaymentButton.addEventListener('click', function() {
+			payType = 'offline';
+			orderData.payment = payType;
+			cashPaymentButton.classList.add('button_alt-active');
+			onlinePaymentButton.classList.remove('button_alt-active');
+			console.log('paytype:' + payType);
+		});
+		addressInput.addEventListener('input', function() {
+			if (addressInput.value.trim() != '') {
+				orderData.address = addressInput.value.trim();
+				nextButton.removeAttribute('disabled');
+				errorSpan.textContent = '';
+			} else {
+				nextButton.setAttribute('disabled', 'true');
+				errorSpan.textContent = 'Пожалуйста, введите адрес доставки';
+			}
+		});
+		nextButton.addEventListener('click', function() {
+			console.log('Далее');
 			modalContent.innerHTML = '';
-			const orderContent = orderTemplate.content.cloneNode(true);
-			modalContent.appendChild(orderContent);
+			const contactsContent = contactsTemplate.content.cloneNode(true);
+			modalContent.appendChild(contactsContent);
 			modalContainer.classList.add('modal_active');
-			modalContainer.style.position = "fixed";
-			const orderForm:HTMLFormElement = modalContent.querySelector('.order');
-			const onlinePaymentButton = orderForm.querySelector('button[name="card"]');
-			const cashPaymentButton = orderForm.querySelector('button[name="cash"]');
-			const addressInput:HTMLInputElement = modalContent.querySelector('.form__input');
-			const errorSpan = modalContent.querySelector('.form__errors');
-			const nextButton = modalContent.querySelector('.order__button');
-			let payType = 'online';
-			onlinePaymentButton.addEventListener('click', function() {
-				payType = 'online';
-				orderData.payment = payType;
-				onlinePaymentButton.classList.add('button_alt-active');
-				cashPaymentButton.classList.remove('button_alt-active');
-				console.log('paytype:' + payType);
-			});
-			cashPaymentButton.addEventListener('click', function() {
-				payType = 'offline';
-				orderData.payment = payType;
-				cashPaymentButton.classList.add('button_alt-active');
-				onlinePaymentButton.classList.remove('button_alt-active');
-				console.log('paytype:' + payType);
-			});
-			addressInput.addEventListener('input', function(){
-				if (addressInput.value.trim() != '') {
-					orderData.address = addressInput.value.trim();
-					nextButton.removeAttribute('disabled');
-					errorSpan.textContent = '';
+			modalContainer.style.position = 'fixed';
+			const emailInput: HTMLInputElement = modalContent.querySelector('input[name="email"]');
+			const phoneInput: HTMLInputElement = modalContent.querySelector('input[name="phone"]');
+			const contactsForm: HTMLFormElement = modalContent.querySelector('.form');
+			const submitButton: HTMLButtonElement = modalContent.querySelector('button[type="submit"]');
+			const contactsErrorSpan: HTMLSpanElement = modalContent.querySelector('.form__errors');
+
+			emailInput.addEventListener('input', function() {
+				if (emailInput.checkValidity() && phoneInput.checkValidity() && emailInput.value.trim() !== '' && phoneInput.value.trim() !== '') {
+					orderData.email = emailInput.value.trim();
+					submitButton.removeAttribute('disabled');
+					contactsErrorSpan.textContent = '';
 				} else {
-					nextButton.setAttribute('disabled', 'true');
-					errorSpan.textContent = 'Пожалуйста, введите адрес доставки';
+					submitButton.setAttribute('disabled', 'true');
+					contactsErrorSpan.textContent = 'Пожалуйста, заполните все поля и укажите корректные данные';
+					orderData.email = emailInput.value.trim();
 				}
 			});
-			nextButton.addEventListener('click', function() {
-				console.log('Далее');
-				modalContent.innerHTML = '';
-				const contactsContent = contactsTemplate.content.cloneNode(true);
-				modalContent.appendChild(contactsContent);
-				modalContainer.classList.add('modal_active');
-				modalContainer.style.position = "fixed";
-				const emailInput: HTMLInputElement = modalContent.querySelector('input[name="email"]');
-				const phoneInput: HTMLInputElement = modalContent.querySelector('input[name="phone"]');
-				const contactsForm: HTMLFormElement = modalContent.querySelector('.form');
-				const submitButton: HTMLButtonElement = modalContent.querySelector('button[type="submit"]');
-				const contactsErrorSpan: HTMLSpanElement = modalContent.querySelector('.form__errors');
 
-				emailInput.addEventListener('input', function() {
-					if (emailInput.checkValidity() && phoneInput.checkValidity() && emailInput.value.trim() !== '' && phoneInput.value.trim() !== '') {
-						orderData.email = emailInput.value.trim();
-						submitButton.removeAttribute('disabled');
-						contactsErrorSpan.textContent = '';
-					} else {
-						submitButton.setAttribute('disabled', 'true');
-						contactsErrorSpan.textContent = 'Пожалуйста, заполните все поля и укажите корректные данные';
-						orderData.email = emailInput.value.trim();
+			phoneInput.addEventListener('input', function() {
+				if (emailInput.checkValidity() && phoneInput.checkValidity() && emailInput.value.trim() !== '' && phoneInput.value.trim() !== '') {
+					submitButton.removeAttribute('disabled');
+					contactsErrorSpan.textContent = '';
+					orderData.phone = phoneInput.value.trim();
+				} else {
+					submitButton.setAttribute('disabled', 'true');
+					contactsErrorSpan.textContent = 'Пожалуйста, заполните все поля и укажите корректные данные';
+				}
+			});
+			submitButton.addEventListener('click', function(event) {
+				event.preventDefault();
+				console.log('Данные заказа:', orderData);
+				api.post('/order/', orderData).then(response => {
+					console.log(JSON.stringify(response));
+					modalContent.innerHTML = '';
+					const successTemplate = document.getElementById('success') as HTMLTemplateElement;
+					const successContent = successTemplate.content.cloneNode(true) as HTMLElement;
+					modalContent.appendChild(successContent);
+					const descriptionElement = modalContent.querySelector('.order-success__description');
+					if (descriptionElement) {
+						descriptionElement.textContent = `Списано ${totalPrice} синапсов`;
 					}
-				});
-
-				phoneInput.addEventListener('input', function() {
-					if (emailInput.checkValidity() && phoneInput.checkValidity() && emailInput.value.trim() !== '' && phoneInput.value.trim() !== '') {
-						submitButton.removeAttribute('disabled');
-						contactsErrorSpan.textContent = '';
-						orderData.phone = phoneInput.value.trim();
-					} else {
-						submitButton.setAttribute('disabled', 'true');
-						contactsErrorSpan.textContent = 'Пожалуйста, заполните все поля и укажите корректные данные';
-					}
-				});
-				submitButton.addEventListener('click', function(event) {
-					event.preventDefault();
-					console.log('Данные заказа:', orderData);
-					fetch('https://larek-api.nomoreparties.co/api/weblarek/order', {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify(orderData),
-					})
-						.then(response => {
-							modalContent.innerHTML = '';
-							const successTemplate = document.getElementById('success') as HTMLTemplateElement;
-							const successContent = successTemplate.content.cloneNode(true) as HTMLElement;
-							modalContent.appendChild(successContent);
-							const descriptionElement = modalContent.querySelector('.order-success__description');
-							if (descriptionElement) {
-								descriptionElement.textContent = `Списано ${totalPrice} синапсов`;
-							}
-							const closeModalButton = modalContent.querySelector('.order-success__close');
-							closeModalButton.addEventListener("click", function(event) {
-								modalContainer.classList.remove('modal_active');
-								modalContainer.style.position = "fixed";
-							});
-							basket.length = 0;
-							upateBasketCounter();
-							totalPrice = 0;
-						})
-						.then(data => {
-							console.log('Ответ сервера:', data);
-						})
-						.catch(error => {
-							console.error('Ошибка:', error);
-						});
+					const closeModalButton = modalContent.querySelector('.order-success__close');
+					closeModalButton.addEventListener('click', function(event) {
+						modalContainer.classList.remove('modal_active');
+						modalContainer.style.position = 'fixed';
+					});
+					basket.length = 0;
+					upateBasketCounter();
+					totalPrice = 0;
 				});
 			});
-		})
+		});
+	});
 }
 
 function upateBasketCounter() {
@@ -304,4 +297,4 @@ const orderTemplate = document.getElementById('order') as HTMLTemplateElement;
 
 let customer: Customer | null = null;
 
-const contactsTemplate:HTMLTemplateElement = document.getElementById('contacts') as HTMLTemplateElement;
+const contactsTemplate: HTMLTemplateElement = document.getElementById('contacts') as HTMLTemplateElement;
